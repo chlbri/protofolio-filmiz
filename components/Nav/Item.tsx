a4201b6d56af9bd7f21a2078adb77d3832c182c6
chlbri <chlbri.blac@gmail.com>
2021-06-24 01:13:44 +0000

    format

--- components/Nav/Item.tsx
+++ components/Nav/Item.tsx
@@ -1,28 +1,36 @@
-import { useRouter } from "next/dist/client/router";
-import React, { ComponentProps, FC } from "react";
+import { useRouter } from 'next/dist/client/router';
+import React, { ComponentProps, FC } from 'react';
+import requests from '../../utils/requests';
 
 export type ItemProps = {
   title: string;
   url: string;
 };
 
-const Item: FC<ComponentProps<"div"> & ItemProps> = ({ id, title, url }) => {
+const Item: FC<ComponentProps<'div'> & ItemProps> = ({
+  id,
+  title,
+  url,
+}) => {
+  // #region Preparing
+
   const route = `/?genre=${id}`;
   const router = useRouter();
-  let className = `last:pr-24 cursor-pointer  transition duration-100 transform hover:scale-125 ${
-    route === router.asPath ? "text-pink-600" : "hover:text-white"
+  const pinkCondition =
+    route === router.asPath ||
+    (title === requests.fetchTrending.title && router === '/');
+
+  // #endregion
+
+  const className = `last:pr-24 cursor-pointer  transition duration-100 transform hover:scale-125 ${
+    pinkCondition ? 'text-pink-600' : 'hover:text-white'
   } active:text-red-400`;
 
-  return (
-    <h2
-      onClick={() => {
-        router.push(route);
-      }}
-      className={className}
-    >
-      {title}
-    </h2>
-  );
+  const onClick = () => {
+    router.push(route);
+  };
+
+  return <h2 {...{ onClick, className }}>{title}</h2>;
 };
 
 export default Item;
