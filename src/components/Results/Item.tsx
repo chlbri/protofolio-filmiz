fb8e1e70b8b2cbb31d2949cbd3ce12a3e0deca6f
chlbri <chlbri.blac@gmail.com>
2021-07-28 03:50:07 +0000

    add hoook

--- src/components/Results/Item.tsx
+++ src/components/Results/Item.tsx
@@ -44,37 +44,41 @@ const ItemResult = forwardRef<
         className='group cursor-pointer pt-5 mx-3 transition duration-200 ease-in transform sm:hover:scale-105  drop-shadow-md'
         {...{ onClick, ref }}
       >
-        <Tippy
-          disabled={disabled}
-          content={title || original_title}
-          theme='tooltip'
-          delay={[700, 100]}
-          duration={[1000, 0]}
-          className='md:-mb-3'
-        >
-          <div
-            ref={yRef}
-            className='truncate object-contain text-2xl mb-5 py-1 sm:text-3xl text-center text-white transition-all duration-100 ease-in-out group-hover:font-bold'
+        <div className='group'>
+          <Tippy
+            disabled={disabled}
+            content={title || original_title}
+            theme='tooltip'
+            delay={[700, 100]}
+            duration={[1000, 0]}
+            className='md:-mb-3'
           >
-            {title || original_title}
-          </div>
-        </Tippy>
+            <div
+            onMouseOverCapture={()=>{}}
+              ref={yRef}
+              
+              className='truncate object-contain text-2xl mb-5 py-1 sm:text-3xl text-center text-white transition-all duration-100 ease-in-out group-hover:font-bold'
+            >
+              {title || original_title}
+            </div>
+          </Tippy>
 
-        <Image
-          className='mt-5 rounded-lg'
-          layout='responsive'
-          src={src}
-          width={1920}
-          height={1080}
-          alt={original_title}
-          objectFit='cover'
-        />
-        <div>
-          <p className='truncate max-w-md px-5'>{overview}</p>
-          <p className='flex px-5 items-center opacity-0 group-hover:opacity-75'>
-            {!!release_date && `${release_date} •`}{' '}
-            <ThumbUpIcon className='h-5 mx-2' /> {vote_count}
-          </p>
+          <Image
+            className='mt-5 rounded-lg'
+            layout='responsive'
+            src={src}
+            width={1920}
+            height={1080}
+            alt={original_title}
+            objectFit='cover'
+          />
+          <div>
+            <p className='truncate max-w-md px-5'>{overview}</p>
+            <p className='flex px-5 items-center opacity-0 group-hover:opacity-75'>
+              {!!release_date && `${release_date} •`}{' '}
+              <ThumbUpIcon className='h-5 mx-2' /> {vote_count}
+            </p>
+          </div>
         </div>
       </div>
     </>
--- src/hooks/useIsOverflowed.ts
+++ src/hooks/useIsOverflowed.ts
@@ -1,12 +1,22 @@
-import { useCallback, useLayoutEffect, useRef, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  createRef,
+  useState,
+} from 'react';
 import useResize from './useResize';
 
 const useIsOverflowed = () => {
   const [disabled, setDisabled] = useState(true);
-  const ref = useRef<any>();
+  const ref = useRef<HTMLDivElement>(null);
 
   const update = useCallback(() => {
-    setDisabled(ref.current.scrollWidth <= ref.current.clientWidth);
+    console.log('Updated');
+    const out =
+      !!ref.current && ref.current.scrollWidth <= ref.current.clientWidth;
+    setDisabled(out);
   }, [setDisabled]);
 
   useLayoutEffect(() => {
@@ -16,7 +26,20 @@ const useIsOverflowed = () => {
     update();
   }, [update]);
 
-  useResize(update, ref.current);
+  useLayoutEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    function updt() {
+      console.log('Hover !!!! and CO&');
+      update();
+    }
+    ref.current!.addEventListener('mouseover', updt);
+    return ref.current!.removeEventListener('mouseover', updt);
+  }, [update, ref]);
+
+  useResize(update, !!ref.current);
   return [ref, disabled, setDisabled] as const;
 };
 
