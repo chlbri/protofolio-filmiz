ceb1367ae2a3d95a79f0be5a2b1b091fde69aedd
chlbri <bri_lvi@icloud.com>
2022-02-27 13:13:06 +0000

    some mis

--- src/lib/abr/store.ts
+++ src/lib/abr/store.ts
@@ -8,7 +8,8 @@ import create, {
 import zx, { Store } from "@bemedev/middleware-zustand-xstate-fsm";
 import { useRouter } from "next/router";
 import { TContext, TEvent } from "./types";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
+import { StateMachine } from "@xstate/fsm";
 
 const _useAppMachine = create(zx(machine));
 
@@ -24,6 +25,19 @@ type _StateSelector<U = any> = StateSelector<
   U
 >;
 
+function getSub(
+  store: Store<
+    TContext,
+    TEvent,
+    {
+      value: any;
+      context: TContext;
+    }
+  >
+) {
+  return store.service.subscribe;
+}
+
 function useAppMachine<U = any>(
   selector?: _StateSelector<U>,
   equalityFn?: EqualityChecker<U>
@@ -32,19 +46,34 @@ function useAppMachine<U = any>(
   //   const genre = _useAppMachine((store) => store.state.context.genre);
   //   const lang = _useAppMachine((store) => store.state.context.language);
   //   const changed = _useAppMachine((store) => store.state.changed);
-  const subscribe = _useAppMachine((store) => store.service.subscribe);
+  const subscribe = _useAppMachine(getSub);
+
+  const _sub = useMemo(
+    () =>
+      (
+        state: StateMachine.State<
+          TContext,
+          TEvent,
+          {
+            value: any;
+            context: TContext;
+          }
+        >
+      ) => {
+        if (state.changed)
+          //   push({
+          //     pathname: "/",
+          //     query: {
+          //       genre: state.context.genre,
+          //       lang: state.context.language,
+          //     },
+          //   });
+          console.log("count");
+      },
+    []
+  );
   useEffect(() => {
-    subscribe((state) => {
-      if (state.changed)
-        push({
-          pathname: "/",
-          query: {
-            genre: state.context.genre,
-            lang: state.context.language,
-          },
-        });
-      console.log("count");
-    });
+    subscribe(_sub);
   }, []);
 
   const _selector =
