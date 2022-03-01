70e33f07b4446cf862518d310d9fabaf0e351549
chlbri <bri_lvi@icloud.com>
2022-03-01 01:13:24 +0000

    fix: Lock SCroll

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,12 +1,26 @@
 import { ThumbUpIcon } from '@heroicons/react/outline';
-import type { FC } from 'react';
+import { FC, MutableRefObject } from 'react';
 import useModal from '../hooks/useModal';
+import Movie from '../lib/ebr/Movie';
 
-const Modal: FC = () => {
-  const { movie, refC, onClick } = useModal();
+type _Props = Pick<
+  Movie,
+  'title' | 'overview' | 'release_date' | 'vote_count'
+> & {
+  refC: MutableRefObject<any>;
+  onClick: () => void;
+};
 
-  return !movie ? null : (
-    <div className="">
+const _Modal: FC<_Props> = ({
+  title,
+  overview,
+  release_date,
+  vote_count,
+  refC,
+  onClick,
+}) => {
+  return (
+    <>
       <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none ">
         <div className="relative my-6 mx-2 max-h-screen w-auto max-w-3xl overflow-hidden rounded-2xl bg-[#06202A] text-white md:mx-auto">
           {/*content*/}
@@ -17,23 +31,22 @@ const Modal: FC = () => {
             {/*header*/}
             <div className="border-blueGray-200 flex items-center justify-center border-b border-solid p-3 md:p-5">
               <h3 className="text-center text-2xl font-semibold md:text-3xl">
-                {movie.title}
+                {title}
               </h3>
             </div>
             {/*body*/}
             <div className="relative max-h-96 overflow-y-auto p-2 md:max-h-screen">
               <p className="my-3 px-5 leading-relaxed md:text-lg">
-                {!movie.overview || movie.overview.trim() === ''
+                {!overview || overview.trim() === ''
                   ? 'Pas de description'
-                  : movie.overview}
+                  : overview}
               </p>
             </div>
             {/*footer*/}
             <div className="border-blueGray-200 flex items-center  justify-between rounded-b border-t border-solid pr-4 pt-3 pb-4 text-sm md:pl-10 md:pr-10 md:text-lg">
               <p className="flex items-center px-5 opacity-75">
-                {!!movie.release_date && `${movie.release_date} •`}{' '}
-                <ThumbUpIcon className="mx-1 h-5 md:mx-2" />{' '}
-                {movie.vote_count}
+                {!!release_date && `${release_date} •`}{' '}
+                <ThumbUpIcon className="mx-1 h-5 md:mx-2" /> {vote_count}
               </p>
               <button
                 className=" background-transparent font-bold uppercase text-red-500  outline-none transition-all duration-150 ease-linear focus:outline-none"
@@ -46,8 +59,14 @@ const Modal: FC = () => {
         </div>
       </div>
       <div className="fixed inset-0 z-20 bg-black opacity-70"></div>
-    </div>
+    </>
   );
+};
+
+const Modal: FC = () => {
+  const { movie, refC, onClick } = useModal();
+
+  return !movie ? null : <_Modal {...{ ...movie, refC, onClick }} />;
 };
 
 export default Modal;
