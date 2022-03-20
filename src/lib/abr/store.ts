d06733884fb618de7787926b059bf0e90c88a398
chlbri <bri_lvi@icloud.com>
2022-03-20 12:11:53 +0000

    fix: suppress store

--- src/lib/abr/store.ts
+++
@@ -1,7 +0,0 @@
-import zx from 'zustand-middleware-xstate';
-import create from 'zustand';
-import { machine } from './machine';
-
-const useAppMachine = create(zx(machine));
-
-export default useAppMachine;
